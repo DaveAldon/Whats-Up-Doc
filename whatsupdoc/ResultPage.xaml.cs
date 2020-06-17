acb7d965a82ccbf49c29e0e1f0aa509aaa87994c
DaveAldon <crawford@mail.gvsu.edu>
2020-06-17 02:58:58 -0400

    ResultPage now displays the parsed provider list. Moved search logic from SearchPage to ResultPage. Clicking on a provider now goes to the ProviderPage.

--- whatsupdoc/ResultPage.xaml.cs
+++ whatsupdoc/ResultPage.xaml.cs
@@ -1,23 +1,97 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using Newtonsoft.Json.Linq;
 using Xamarin.Forms;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace whatsupdoc
 {
     public partial class ResultPage : ContentPage
     {
+        // List of provider objects for the listview
+        IList<Provider> Providers = new List<Provider>();
+
         public ResultPage()
         {
             InitializeComponent();
         }
 
-        void SearchBar_TextChanged(System.Object sender, Xamarin.Forms.TextChangedEventArgs e)
+        protected override void OnAppearing()
         {
+            base.OnAppearing();
+            Providers = new List<Provider>();
+
+            var o = Utils.GetFHIRResource();
+            JObject results = JObject.Parse(o);
+
+            // Parent structure
+            var entries = results["entry"];
+
+            // Iter through the providers
+            foreach (var entry in entries)
+            {
+                try
+                {
+                    // Parsing out certain fields
+                    var pName = entry["resource"]["practitioner"]["display"].ToString();
+                    // Names have numbers in the sample data. This makes them prettier
+                    pName = Regex.Replace(pName, @"[\d-]", string.Empty);
+
+                    var pOrganization = entry["resource"]["organization"]["display"].ToString();
+                    //var pSpecialty = entry["resource"]["specialty"][1]["coding"][0]["display"].ToString();
+
+                    // Add them to a new provider object
+                    Providers.Add(new Provider
+                    {
+                        ProviderName = pName,
+                        ProviderOrganization = pOrganization,
+                        //ProviderSpecialty = pSpecialty
+                    });
+                }
+                catch (Exception exc)
+                {
+                    Console.WriteLine(exc);
+                }
+            }
+
+            listView.ItemsSource = Providers
+                .OrderBy(d => d.ProviderName)
+                .ToList();
+
+            // Troubleshooting that displays all of the provider objects
+            /*
+            foreach(var x in Providers)
+            {
+                await DisplayAlert("Alert", x.ProviderName + " " + x.ProviderOrganization + " " + x.ProviderSpecialty, "OK");
+            }
+            */
         }
 
-        void Clicked_Selected_Provider(System.Object sender, Xamarin.Forms.SelectedItemChangedEventArgs e)
+        // Search bar filtering
+        void SearchBar_TextChanged(System.Object sender, Xamarin.Forms.TextChangedEventArgs e)
         {
+            listView.BeginRefresh();
+            SearchBar searchBar = (SearchBar)sender;
+            if (string.IsNullOrWhiteSpace(e.NewTextValue))
+                listView.ItemsSource = Providers.OrderBy(d => d.ProviderName).ToList();
+            else
+                listView.ItemsSource = Providers.OrderBy(d => d.ProviderName).ToList()
+                    .Where(i => i.ProviderName.IndexOf(e.NewTextValue, StringComparison.OrdinalIgnoreCase) != -1);
+
+            listView.EndRefresh();
+        }
+
+        // User selects a provider
+        async void Clicked_Selected_Provider(System.Object sender, Xamarin.Forms.SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem != null)
+            {
+                await Navigation.PushAsync(new ProviderPage
+                {
+                    BindingContext = this.BindingContext
+                });
+            }
         }
     }
 }
--- whatsupdoc/SearchPage.xaml.cs
+++ whatsupdoc/SearchPage.xaml.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using Newtonsoft.Json.Linq;
+﻿using System.ComponentModel;
 using Xamarin.Forms;
 
 namespace whatsupdoc
@@ -9,9 +6,6 @@ namespace whatsupdoc
     [DesignTimeVisible(false)]
     public partial class SearchPage : ContentPage
     {
-        // List of provider objects for the listview
-        IList<Provider> Providers = new List<Provider>();
-
         public SearchPage()
         {
             InitializeComponent();
@@ -23,50 +17,14 @@ namespace whatsupdoc
 
         async void Search_Button_Clicked(System.Object sender, System.EventArgs e)
         {
-            var o = Utils.GetFHIRResource();
-            JObject results = JObject.Parse(o);
 
-            // Parent structure
-            var entries = results["entry"];
-
-            // Iter through the providers
-            foreach(var entry in entries)
-            {
-                try
-                {
-                    // Parsing out certain fields
-                    var pName = entry["resource"]["practitioner"]["display"].ToString();
-                    var pOrganization = entry["resource"]["organization"]["display"].ToString();
-                    //var pSpecialty = entry["resource"]["specialty"][1]["coding"][0]["display"].ToString();
-
-                    // Add them to a new provider object
-                    Providers.Add(new Provider
-                    {
-                        ProviderName =  pName,
-                        ProviderOrganization = pOrganization,
-                        //ProviderSpecialty = pSpecialty
-                    });
-                }
-                catch(Exception exc)
-                {
-                    Console.WriteLine(exc);
-                }
-            }
-
-            // Troubleshooting that displays all of the provider objects
-            foreach(var x in Providers)
-            {
-                await DisplayAlert("Alert", x.ProviderName + " " + x.ProviderOrganization + " " + x.ProviderSpecialty, "OK");
-            }
-            
-            /*
             // Go to listview page and change its binding
             await Navigation.PushAsync(new ResultPage
             {
                 // Updates the proper context for the next view
                 BindingContext = this.BindingContext
             });
-            */
+            
         }
     }
 }
