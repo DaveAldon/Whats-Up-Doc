5b6021944234eeff582a08ed3c1c5d82529d560c
DaveAldon <crawford@mail.gvsu.edu>
2020-06-17 02:36:52 -0400

    Added API connection, Provider object structure, and parsing of a sample query.

---
+++ whatsupdoc/Provider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace whatsupdoc
+{
+    public class Provider
+    {
+        public string ProviderName { get; set; }
+        public string ProviderSpecialty { get; set; }
+        public string ProviderOrganization { get; set; }
+
+        public override string ToString()
+        {
+            return ProviderSpecialty;
+        }
+    }
+}
--- whatsupdoc/Utils.cs
+++ whatsupdoc/Utils.cs
@@ -1,11 +1,33 @@
-﻿using System;
+﻿using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Hl7.Fhir.Rest;
+
 namespace whatsupdoc
 {
     // Class to hold static api logic and configs
-    public class Utils
+    public static class Utils
     {
-        public Utils()
+        public static string FHIR_Resource = "http://hapi.fhir.org/baseR4/";
+        public static string ProviderQuery = "PractitionerRole?specialty=208D00000X&_include=PractitionerRole:organization&_include=PractitionerRole:location";
+
+        public static string GetProvidersAPI()
         {
+            return FHIR_Resource + ProviderQuery;
+        }
+
+        public static string GetFHIRResource()
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(GetProvidersAPI());
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
         }
     }
 }
